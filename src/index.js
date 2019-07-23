0fa84fb9c7a31a831f8d0f4d257ae6c9fb767209
gedrimas <a.a.gedrimas@gmail.com>
2019-07-23 23:11:40 +0300

    fix rout bug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import 'dotenv/config'
-import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
+import bodyParser from 'body-parser'
+import express from 'express'
+
 import models from './models'
 import routes from './routes'
 
 const app = express();
 
 app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,9 +21,10 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use('./session', routes.session)
-app.use('./user', routes.user)
-app.use('./messages', routes.message)
+
+app.use('/session', routes.session)
+app.use('/user', routes.user)
+app.use('/messages', routes.message)
 
 app.listen(process.env.PORT, () => 
   console.log(`Examle app listening on port ${process.env.PORT}!`),
